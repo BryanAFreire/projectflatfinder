70f43a203f6e9ec765c3288770057405af790f42
Bryan Freire <bfreire@krugerschool.edu.ec>
2024-10-27 08:18:19 -0500

    deleted nav pagination and updated just for get data from flats_favorites

--- asserts/events/home.js
+++ asserts/events/home.js
@@ -4,28 +4,33 @@ import { listProfile } from './utils/profileMenu.js';
 import { dataFavoriteFlat } from './utils/getFavoriteFlats.js';
 import { changeMenuBurger } from "./utils/burgerMenu.js";
 import {logOut} from "./utils/logOut.js";
+import {checkTokenExpiration} from "./utils/checkingExpirationToken.js";
+import {validateSession} from "./utils/verifySession.js";
 
 document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault()
-
+    const token = localStorage.getItem('token');
+    
+    validateSession(token);
+    
+    logOut();
+    
     userGreeting();
-
+    
     document.addEventListener('click', displayNoneMenuPerfil);
-
-    const logOutLink = document.querySelector('#logout_link');
-    if(logOutLink){
-        logOutLink.addEventListener('click', logOut)
-    }
-
+    
     const profileIco = document.querySelector('.content-profile');
     if (profileIco) {
         profileIco.addEventListener('click', listProfile)
     }
-
+    
     const burgerMenu = document.querySelector('.burger-menu');
     if (burgerMenu) {
         burgerMenu.addEventListener('click', changeMenuBurger);
     }
+    
+    // Check token expiration every second
+    setInterval(() => checkTokenExpiration(token), 1000);
 
     dataFavoriteFlat();
 
